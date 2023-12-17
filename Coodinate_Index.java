79f9f7b1251479b376bb0bfb976ecd7f31184d4c
Atsushi-1207 <153877597+Atsushi-1207@users.noreply.github.com>
2023-12-17 16:20:23 +0900

    Add files via upload
    start

---
+++ Coodinate_Index.java
@@ -0,0 +1,121 @@
+public class Coodinate_Index {
+    static private double Coodinate[][] = new double[7][7];
+
+    public static void SetCoodinate(int x, int y, int state) {
+        // ハズレの座標index処理
+        if (state == 0) {
+            Coodinate[x][y] = 0;
+            // 最左列を排除
+            if (x != 1) {
+                Coodinate[x - 1][y] = 0;
+                // 最上列を排除
+                if (y != 1) {
+                    Coodinate[x - 1][y + 1] = 0;
+                }
+                // 最下列を排除
+                if (y != 5) {
+                    Coodinate[x - 1][y - 1] = 0;
+                }
+            }
+            // 最右列を排除
+            if (x != 5) {
+                Coodinate[x + 1][y] = 0;
+                // 最上列を排除
+                if (y != 1) {
+                    Coodinate[x + 1][y + 1] = 0;
+                }
+                // 最下列を排除
+                if (y != 5) {
+                    Coodinate[x + 1][y - 1] = 0;
+                }
+            }
+            // 最上列を排除
+            if (y != 1) {
+                Coodinate[x][y + 1] = 0;
+            }
+            // 最下列を排除
+            if (y != 5) {
+                Coodinate[x][y - 1] = 0;
+            }
+        }
+
+        // 波高しの座標index処理
+        if (state == 1) {
+            Coodinate[x][y] += 0.1;
+            // 最左列を排除
+            if (x != 1) {
+                Coodinate[x - 1][y] += 0.1;
+                // 最上列を排除
+                if (y != 1) {
+                    Coodinate[x - 1][y + 1] += 0.1;
+                }
+                // 最下列を排除
+                if (y != 5) {
+                    Coodinate[x - 1][y - 1] += 0.1;
+                }
+            }
+            // 最右列を排除
+            if (x != 5) {
+                Coodinate[x + 1][y] += 0.1;
+                // 最上列を排除
+                if (y != 1) {
+                    Coodinate[x + 1][y + 1] += 0.1;
+                }
+                // 最下列を排除
+                if (y != 5) {
+                    Coodinate[x + 1][y - 1] += 0.1;
+                }
+            }
+            // 最上列を排除
+            if (y != 1) {
+                Coodinate[x][y + 1] += 0.1;
+            }
+            // 最下列を排除
+            if (y != 5) {
+                Coodinate[x][y - 1] += 0.1;
+            }
+        }
+
+        // ヒットの座標index処理
+        if (state == 2) {
+            Coodinate[x][y] += 1;
+            // 最左列を排除
+            if (x != 1) {
+                Coodinate[x - 1][y] = 0;
+                // 最上列を排除
+                if (y != 1) {
+                    Coodinate[x - 1][y + 1] = 0;
+                }
+                // 最下列を排除
+                if (y != 5) {
+                    Coodinate[x - 1][y - 1] = 0;
+                }
+            }
+            // 最右列を排除
+            if (x != 5) {
+                Coodinate[x + 1][y] = 0;
+                // 最上列を排除
+                if (y != 1) {
+                    Coodinate[x + 1][y + 1] = 0;
+                }
+                // 最下列を排除
+                if (y != 5) {
+                    Coodinate[x + 1][y - 1] = 0;
+                }
+            }
+            // 最上列を排除
+            if (y != 1) {
+                Coodinate[x][y + 1] = 0;
+            }
+            // 最下列を排除
+            if (y != 5) {
+                Coodinate[x][y - 1] = 0;
+            }
+        }
+    }
+
+    public static double GetCoodinate(int x, int y) {
+        return Coodinate[x][y];
+    }
+
+}
---
+++ Naval_Game_main.java
@@ -0,0 +1,9 @@
+public class Naval_Game_main {
+    public static void main(String[] args) {
+
+        // ハズレ=0,波高し=1,ヒット=2
+        int state = 0;
+        
+        
+    }
+}
