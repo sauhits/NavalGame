e2d9f04d767d7afafa9702636fb43e745add6f6e
Atsushi-1207 <atsushi.obata1207@gmail.com>
2023-12-22 03:41:03 +0900

    NewGameの実装 成形

--- Naval_Game_main.java
+++ Naval_Game_main.java
@@ -1,9 +1,6 @@
 public class Naval_Game_main {
     public static void main(String[] args) {
-
-        // ハズレ=0,波高し=1,ヒット=2
-        int state = 0;
-        
+        new New_Game();
         
     }
 }
