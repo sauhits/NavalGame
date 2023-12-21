21960a653c892d1f4f811b08949b724f7105a201
Atsushi-1207 <atsushi.obata1207@gmail.com>
2023-12-22 05:39:58 +0900

    SelfCoodinateのget,setの作成

--- New_Game.java
+++ New_Game.java
@@ -1,9 +1,9 @@
 public class New_Game {
     public static void NewGame() {
         for (int i = 0; i < 6; i++) {
-            VariableDB.SetCoodinate(i, 1, 0);
-            VariableDB.SetCoodinate(i, 3, 0);
-            VariableDB.SetCoodinate(i, 5, 0);
+            VariableDB.SetEnemyCoodinate(i, 1, 0);
+            VariableDB.SetEnemyCoodinate(i, 3, 0);
+            VariableDB.SetEnemyCoodinate(i, 5, 0);
         }
     }
 }
--- VariableDB.java
+++ VariableDB.java
@@ -1,122 +1,140 @@
 import java.util.Set;
 
 public class VariableDB {
-    static private double Coodinate[][] = new double[7][7];
+    static private double EnemyCoodinate[][] = new double[7][7];
+    static private double SelfCoodinate[][] = new double[7][7];
 
-    public static void SetCoodinate(int x, int y, int state) {
+    public static void SetEnemyCoodinate(int x, int y, int state) {
         // ハズレの座標index処理
         if (state == 0) {
-            Coodinate[x][y] = 0;
+            EnemyCoodinate[x][y] = 0;
             // 最左列を排除
             if (x != 1) {
-                Coodinate[x - 1][y] = 0;
+                EnemyCoodinate[x - 1][y] = 0;
                 // 最上列を排除
                 if (y != 1) {
-                    Coodinate[x - 1][y + 1] = 0;
+                    EnemyCoodinate[x - 1][y + 1] = 0;
                 }
                 // 最下列を排除
                 if (y != 5) {
-                    Coodinate[x - 1][y - 1] = 0;
+                    EnemyCoodinate[x - 1][y - 1] = 0;
                 }
             }
             // 最右列を排除
             if (x != 5) {
-                Coodinate[x + 1][y] = 0;
+                EnemyCoodinate[x + 1][y] = 0;
                 // 最上列を排除
                 if (y != 1) {
-                    Coodinate[x + 1][y + 1] = 0;
+                    EnemyCoodinate[x + 1][y + 1] = 0;
                 }
                 // 最下列を排除
                 if (y != 5) {
-                    Coodinate[x + 1][y - 1] = 0;
+                    EnemyCoodinate[x + 1][y - 1] = 0;
                 }
             }
             // 最上列を排除
             if (y != 1) {
-                Coodinate[x][y + 1] = 0;
+                EnemyCoodinate[x][y + 1] = 0;
             }
             // 最下列を排除
             if (y != 5) {
-                Coodinate[x][y - 1] = 0;
+                EnemyCoodinate[x][y - 1] = 0;
             }
         }
 
         // 波高しの座標index処理
         if (state == 1) {
-            Coodinate[x][y] += 0.1;
+            EnemyCoodinate[x][y] += 0.1;
             // 最左列を排除
             if (x != 1) {
-                Coodinate[x - 1][y] += 0.1;
+                EnemyCoodinate[x - 1][y] += 0.1;
                 // 最上列を排除
                 if (y != 1) {
-                    Coodinate[x - 1][y + 1] += 0.1;
+                    EnemyCoodinate[x - 1][y + 1] += 0.1;
                 }
                 // 最下列を排除
                 if (y != 5) {
-                    Coodinate[x - 1][y - 1] += 0.1;
+                    EnemyCoodinate[x - 1][y - 1] += 0.1;
                 }
             }
             // 最右列を排除
             if (x != 5) {
-                Coodinate[x + 1][y] += 0.1;
+                EnemyCoodinate[x + 1][y] += 0.1;
                 // 最上列を排除
                 if (y != 1) {
-                    Coodinate[x + 1][y + 1] += 0.1;
+                    EnemyCoodinate[x + 1][y + 1] += 0.1;
                 }
                 // 最下列を排除
                 if (y != 5) {
-                    Coodinate[x + 1][y - 1] += 0.1;
+                    EnemyCoodinate[x + 1][y - 1] += 0.1;
                 }
             }
             // 最上列を排除
             if (y != 1) {
-                Coodinate[x][y + 1] += 0.1;
+                EnemyCoodinate[x][y + 1] += 0.1;
             }
             // 最下列を排除
             if (y != 5) {
-                Coodinate[x][y - 1] += 0.1;
+                EnemyCoodinate[x][y - 1] += 0.1;
             }
         }
 
         // ヒットの座標index処理
         if (state == 2) {
-            Coodinate[x][y] += 1;
+            EnemyCoodinate[x][y] += 1;
             // 最左列を排除
             if (x != 1) {
-                Coodinate[x - 1][y] = 0;
+                EnemyCoodinate[x - 1][y] = 0;
                 // 最上列を排除
                 if (y != 1) {
-                    Coodinate[x - 1][y + 1] = 0;
+                    EnemyCoodinate[x - 1][y + 1] = 0;
                 }
                 // 最下列を排除
                 if (y != 5) {
-                    Coodinate[x - 1][y - 1] = 0;
+                    EnemyCoodinate[x - 1][y - 1] = 0;
                 }
             }
             // 最右列を排除
             if (x != 5) {
-                Coodinate[x + 1][y] = 0;
+                EnemyCoodinate[x + 1][y] = 0;
                 // 最上列を排除
                 if (y != 1) {
-                    Coodinate[x + 1][y + 1] = 0;
+                    EnemyCoodinate[x + 1][y + 1] = 0;
                 }
                 // 最下列を排除
                 if (y != 5) {
-                    Coodinate[x + 1][y - 1] = 0;
+                    EnemyCoodinate[x + 1][y - 1] = 0;
                 }
             }
             // 最上列を排除
             if (y != 1) {
-                Coodinate[x][y + 1] = 0;
+                EnemyCoodinate[x][y + 1] = 0;
             }
             // 最下列を排除
             if (y != 5) {
-                Coodinate[x][y - 1] = 0;
+                EnemyCoodinate[x][y - 1] = 0;
             }
         }
     }
 
-    public static double GetCoodinate(int x, int y) {
-        return Coodinate[x][y];
+    public static double GetEnemyCoodinate(int x, int y) {
+        return EnemyCoodinate[x][y];
     }
+
+    public static void SetSelfCoodinate(int x, int y) {
+        SelfCoodinate[x][y] = 3;
+    }
+
+    public static String GetSelfCoodinate() {
+        String result = "SelfNaval";
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (SelfCoodinate[i][j] != 0) {
+                    result = result + "(" + i + "," + j + ")";
+                }
+            }
+        }
+        return result;
+    }
+
 }
