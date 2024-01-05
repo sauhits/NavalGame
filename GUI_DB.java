3fcc0bbdfe6f1ad0a9669b4477282ee03c6fd801
Atsushi-1207 <atsushi.obata1207@gmail.com>
2024-01-06 02:24:43 +0900

    可読性の向上

---
+++ GUI_DB.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+
+public class GUI_DB {
+    static int xEnemy, yEnemy;
+    static int xSelf, ySelf;
+    static boolean flagEnemy = false;
+    static boolean flagSelf = false;
+    static boolean flagStart = false;
+    static int[][] copyEnemyCoodinate = new int[5][5];
+    static int logCapacity = 5;
+    static ArrayList<String> listLog = new ArrayList<String>(logCapacity);
+    static int countLog = 1;
+    static Font fontLog = new Font(20);
+
+    // Enemy:ラベルの作成
+    static Label labelEnemylNaval = Naval.createLabel("EnemyNaval", "Enemy Naval");
+    // Enemy:座標を表示するラベルの作成
+        static Label labelActiveEnemyCoodinate = Naval.createLabel("activeEnemyCoodinate", "none");
+        // 表示記号の作成-----------------------------------
+        static String[][] textsCoodinates = {
+                { "1,1", "2,1", "3,1", "4,1", "5,1" },
+                { "1,2", "2,2", "3,2", "4,2", "5,2" },
+                { "1,3", "2,3", "3,3", "4,3", "5,3" },
+                { "1,4", "2,4", "3,4", "4,4", "5,4" },
+                { "1,5", "2,5", "3,5", "4,5", "5,5" }
+        };
+        // Enemy:Gridpaneの作成
+        static GridPane gridPaneEnemy = new GridPane();
+        // Enemy:CoodinateButtonの作成-----------------------------------
+        static Button[][] buttonEnemyCoodinates = new Button[5][5];
+        // Enemy:stateButtonの作成
+        static Button buttonHit = Naval.createButton("HIT", 83.3, 40.0);
+        static Button buttonNot = Naval.createButton("NOT", 83.3, 40.0);
+        static Button buttonNear = Naval.createButton("NEAR", 83.3, 40.0);
+        // Enemy:moveボタンの作成
+        static Button buttonEnemySouth_1 = Naval.createButton("South_1", 62.5, 40.0);
+        static Button buttonEnemyNorth_1 = Naval.createButton("North:1", 62.5, 40.0);
+        static Button buttonEnemyEast_1 = Naval.createButton("East:1", 62.5, 40.0);
+        static Button buttonEnemyWest_1 = Naval.createButton("West:1", 62.5, 40.0);
+        static Button buttonEnemySouth_2 = Naval.createButton("South:2", 62.5, 40.0);
+        static Button buttonEnemyNorth_2 = Naval.createButton("North:2", 62.5, 40.0);
+        static Button buttonEnemyEast_2 = Naval.createButton("East:2", 62.5, 40.0);
+        static Button buttonEnemyWest_2 = Naval.createButton("West:2", 62.5, 40.0);
+        // Calcurate:ラベルの作成
+        static Label labelCalcurate = Naval.createLabel("calcurate", "Calcurate Now");
+        // Calcurate:ボタンの作成
+        static Button[][] buttonCalcurateCoodinates = new Button[5][5];
+        // Calucurate:グリットの作成
+        static GridPane gridpaneCalcurate = new GridPane();
+        // Self:ラベルの作成
+        static Label labelSelf = Naval.createLabel("labelSelf", "Self Nabel");
+        static Label labelActiveSelfCoodinate = Naval.createLabel("selfCoodinate", "none");
+        // Self:ボタンの作成
+        static Button[][] buttonSelfCoodinates = new Button[5][5];
+        static Button buttonSelfSet = Naval.createButton("SET", 125.0, 40.0);
+        static Button buttonSelfFired = Naval.createButton("BE FIRED", 125.0, 40.0);
+        // Self:gridpaneの作成
+        static GridPane gridPaneSelf = new GridPane();
+        // Log:ラベルの作成
+        static Label labelLog = Naval.createLabel("labelLog", "LOG");
+        // Log:リストの作成
+        for (int i = 0; i < 5; i++) {
+            Naval.listLogAdd("none");
+        }
+        // Log:リストラベルの作成
+        static Label labelLog1 = new Label("none");
+        static Label labelLog2 = new Label("none");
+        static Label labelLog3 = new Label("none");
+        static Label labelLog4 = new Label("none");
+        static Label labelLog5 = new Label("none");
+}
--- Naval.java
+++ Naval.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -12,18 +14,8 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Naval extends Application {
-    static int xEnemy, yEnemy;
-    static int xSelf, ySelf;
-    static int stateEnemy;
-    static boolean flagEnemy = false;
-    static boolean flagSelf = false;
-    static boolean flagStart = false;
-    static int[][] copyEnemyCoodinate = new int[5][5];
-
-    // =====================================================================================================================================
-    // =====================================================================================================================================
     // Coodinateボタンの作成メソッド
-    private Button createButtonCoodinate(String label, int row, int col, Label labelel, boolean isEnemy) {
+    public static Button createButtonCoodinate(String label, int row, int col, Label labelel, boolean isEnemy) {
         Button button = new Button(label);
         button.setFont(new Font(12));
         button.setMinSize(50, 50);
@@ -32,7 +24,7 @@ public class Naval extends Application {
     }
 
     // ラベル作成メソッド
-    private Label createLabel(String labelName, String textOfFirst) {
+    public static Label createLabel(String labelName, String textOfFirst) {
         Label label = new Label(labelName);
         label.setText(textOfFirst);
         label.setFont(new Font(15));
@@ -42,7 +34,7 @@ public class Naval extends Application {
     }
 
     // ボタンの作成メソッド
-    private Button createButton(String text, Double width, Double height) {
+    public static Button createButton(String text, Double width, Double height) {
         Button button = new Button(text);
         button.setPrefSize(width, height);
         return button;
@@ -53,13 +45,13 @@ public class Naval extends Application {
         int affectationX = x + 1;
         int affectationY = y + 1;
         if (isEnemy) {
-            xEnemy = x;
-            yEnemy = y;
-            flagEnemy = true;
+            GUI_DB.xEnemy = x;
+            GUI_DB.yEnemy = y;
+            GUI_DB.flagEnemy = true;
         } else {
-            xSelf = x;
-            ySelf = y;
-            flagSelf = true;
+            GUI_DB.xSelf = x;
+            GUI_DB.ySelf = y;
+            GUI_DB.flagSelf = true;
         }
 
         label.setText("(" + affectationX + "," + affectationY + ")");
@@ -80,10 +72,10 @@ public class Naval extends Application {
         }
         if (switchSetSelf < 5) {
             String textSetSelf;
-            if (flagSelf) {
-                flagSelf = false;
-                VariableDB.newSelfCoodinate(ySelf, xSelf);
-                button[ySelf][xSelf].setStyle("-fx-base: #f4f162");
+            if (GUI_DB.flagSelf) {
+                GUI_DB.flagSelf = false;
+                VariableDB.newSelfCoodinate(GUI_DB.ySelf, GUI_DB.xSelf);
+                button[GUI_DB.ySelf][GUI_DB.xSelf].setStyle("-fx-base: #f4f162");
                 textSetSelf = "現在" + switchSetSelf + "隻";
             } else {
                 textSetSelf = "座標を選択してください";
@@ -96,10 +88,11 @@ public class Naval extends Application {
 
     // Self:Firedのアクションメソッド
     public static void buttonSelfFiredAction(Label label, Button[][] buttons) {
-        if (flagSelf) {
-            flagSelf = false;
-            VariableDB.setSelfHealth(xSelf, ySelf, VariableDB.getSelfHealth(xSelf, ySelf) - 1);
-            buttonStateEnemyAction(xSelf, ySelf, 1, true, label, buttons);
+        if (GUI_DB.flagSelf) {
+            GUI_DB.flagSelf = false;
+            VariableDB.setSelfHealth(GUI_DB.xSelf, GUI_DB.ySelf,
+                    VariableDB.getSelfHealth(GUI_DB.xSelf, GUI_DB.ySelf) - 1);
+            buttonStateEnemyAction(GUI_DB.xSelf, GUI_DB.ySelf, 1, true, label, buttons);
         } else {
             label.setText("座標を選択してください");
         }
@@ -130,7 +123,7 @@ public class Naval extends Application {
                 }
                 if (affectationX >= 0 && affectationX < 5 && affectationY >= 0 && affectationY < 5) {
                     if (VariableDB.getEnemyEvaluation(i, j) != 0)
-                        copyEnemyCoodinate[affectationX][affectationY] = 1;
+                        GUI_DB.copyEnemyCoodinate[affectationX][affectationY] = 1;
                 } else {
                     System.out.println("Error:rangeOfCopy");
                 }
@@ -139,19 +132,19 @@ public class Naval extends Application {
         // Copyをマージ
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                copyEnemyCoodinate[i][j] += VariableDB.getEnemyEvaluation(i, j);
+                GUI_DB.copyEnemyCoodinate[i][j] += VariableDB.getEnemyEvaluation(i, j);
             }
         }
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                VariableDB.setEnemyEvaluation(i, j, copyEnemyCoodinate[i][j]);
+                VariableDB.setEnemyEvaluation(i, j, GUI_DB.copyEnemyCoodinate[i][j]);
             }
         }
         updadeCalcurateCoodinate(button, label);
     }
 
     // Calculate:Coodinateの更新
-    private static void updadeCalcurateCoodinate(Button[][] button, Label label) {
+    public static void updadeCalcurateCoodinate(Button[][] button, Label label) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 button[i][j].setText(String.valueOf(VariableDB.getEnemyEvaluation(i, j)));
@@ -163,106 +156,137 @@ public class Naval extends Application {
     // Enemy:stateのアクションメソッド
     public static void buttonStateEnemyAction(int x, int y, int state, boolean flag, Label label, Button[][] button) {
         if (flag) {
-            flagEnemy = false;
+            GUI_DB.flagEnemy = false;
             VariableDB.setEnemyCoodinate(x, y, state);
             updadeCalcurateCoodinate(button, label);
+            listLogAdd("(" + x + "," + y + ") STATE: " + state);
         } else {
             label.setText("座標を選択してください");
         }
+    }
+
+    // Log:リストの追加
+    public static void listLogAdd(String text) {
+        GUI_DB.listLog.add(text);
+        if (GUI_DB.listLog.size() > GUI_DB.logCapacity) {
+            GUI_DB.listLog.remove(0);
+        }
+
+    }
+
+    // Log:リストラベルの更新
+    public static void updateListLog() {
+
     }
 
     // =====================================================================================================================================
     // =====================================================================================================================================
     @Override
     public void start(Stage stage) {
-        // Enemy:ラベルの作成
-        Label labelEnemylNaval = createLabel("EnemyNaval", "Enemy Naval");
-        // Enemy:座標を表示するラベルの作成
-        Label labelActiveEnemyCoodinate = createLabel("activeEnemyCoodinate", "none");
-        // 表示記号の作成-----------------------------------
-        String[][] textsCoodinates = {
-                { "1,1", "2,1", "3,1", "4,1", "5,1" },
-                { "1,2", "2,2", "3,2", "4,2", "5,2" },
-                { "1,3", "2,3", "3,3", "4,3", "5,3" },
-                { "1,4", "2,4", "3,4", "4,4", "5,4" },
-                { "1,5", "2,5", "3,5", "4,5", "5,5" }
-        };
-        // Enemy:Gridpaneの作成
-        GridPane gridPaneEnemy = new GridPane();
-        // Enemy:CoodinateButtonの作成-----------------------------------
-        Button[][] buttonEnemyCoodinates = new Button[5][5];
-        // Enemy:stateButtonの作成
-        Button buttonHit = createButton("HIT", 83.3, 40.0);
-        Button buttonNot = createButton("NOT", 83.3, 40.0);
-        Button buttonNear = createButton("NEAR", 83.3, 40.0);
-        // Enemy:moveボタンの作成
-        Button buttonEnemySouth_1 =createButton("South_1", 62.5, 40.0);
-        Button buttonEnemyNorth_1 = createButton("North:1", 62.5, 40.0);
-        Button buttonEnemyEast_1 = createButton("East:1", 62.5, 40.0);
-        Button buttonEnemyWest_1 = createButton("West:1", 62.5, 40.0);
-        Button buttonEnemySouth_2 = createButton("South:2", 62.5, 40.0);
-        Button buttonEnemyNorth_2 = createButton("North:2", 62.5, 40.0);
-        Button buttonEnemyEast_2 = createButton("East:2", 62.5, 40.0);
-        Button buttonEnemyWest_2 = createButton("West:2", 62.5, 40.0);
-        // Calcurate:ラベルの作成
-        Label labelCalcurate = createLabel("calcurate", "Calcurate Now");
-        // Calcurate:ボタンの作成
-        Button[][] buttonCalcurateCoodinates = new Button[5][5];
-        // Calucurate:グリットの作成
-        GridPane gridpaneCalcurate = new GridPane();
-        // Self:ラベルの作成
-        Label labelSelf = createLabel("labelSelf", "Self Nabel");
-        Label labelActiveSelfCoodinate = createLabel("selfCoodinate", "none");
-        // Self:ボタンの作成
-        Button[][] buttonSelfCoodinates = new Button[5][5];
-        Button buttonSelfSet = createButton("SET", 125.0,40.0);
-        Button buttonSelfFired =  createButton("BE FIRED", 125.0,40.0);
-        // Self:gridpaneの作成
-        GridPane gridPaneSelf = new GridPane();
+        // // Enemy:ラベルの作成
+        // Label labelEnemylNaval = createLabel("EnemyNaval", "Enemy Naval");
+        // // Enemy:座標を表示するラベルの作成
+        // Label labelActiveEnemyCoodinate = createLabel("activeEnemyCoodinate", "none");
+        // // 表示記号の作成-----------------------------------
+        // String[][] textsCoodinates = {
+        //         { "1,1", "2,1", "3,1", "4,1", "5,1" },
+        //         { "1,2", "2,2", "3,2", "4,2", "5,2" },
+        //         { "1,3", "2,3", "3,3", "4,3", "5,3" },
+        //         { "1,4", "2,4", "3,4", "4,4", "5,4" },
+        //         { "1,5", "2,5", "3,5", "4,5", "5,5" }
+        // };
+        // // Enemy:Gridpaneの作成
+        // GridPane gridPaneEnemy = new GridPane();
+        // // Enemy:CoodinateButtonの作成-----------------------------------
+        // Button[][] buttonEnemyCoodinates = new Button[5][5];
+        // // Enemy:stateButtonの作成
+        // Button buttonHit = createButton("HIT", 83.3, 40.0);
+        // Button buttonNot = createButton("NOT", 83.3, 40.0);
+        // Button buttonNear = createButton("NEAR", 83.3, 40.0);
+        // // Enemy:moveボタンの作成
+        // Button buttonEnemySouth_1 = createButton("South_1", 62.5, 40.0);
+        // Button buttonEnemyNorth_1 = createButton("North:1", 62.5, 40.0);
+        // Button buttonEnemyEast_1 = createButton("East:1", 62.5, 40.0);
+        // Button buttonEnemyWest_1 = createButton("West:1", 62.5, 40.0);
+        // Button buttonEnemySouth_2 = createButton("South:2", 62.5, 40.0);
+        // Button buttonEnemyNorth_2 = createButton("North:2", 62.5, 40.0);
+        // Button buttonEnemyEast_2 = createButton("East:2", 62.5, 40.0);
+        // Button buttonEnemyWest_2 = createButton("West:2", 62.5, 40.0);
+        // // Calcurate:ラベルの作成
+        // Label labelCalcurate = createLabel("calcurate", "Calcurate Now");
+        // // Calcurate:ボタンの作成
+        // Button[][] buttonCalcurateCoodinates = new Button[5][5];
+        // // Calucurate:グリットの作成
+        // GridPane gridpaneCalcurate = new GridPane();
+        // // Self:ラベルの作成
+        // Label labelSelf = createLabel("labelSelf", "Self Nabel");
+        // Label labelActiveSelfCoodinate = createLabel("selfCoodinate", "none");
+        // // Self:ボタンの作成
+        // Button[][] buttonSelfCoodinates = new Button[5][5];
+        // Button buttonSelfSet = createButton("SET", 125.0, 40.0);
+        // Button buttonSelfFired = createButton("BE FIRED", 125.0, 40.0);
+        // // Self:gridpaneの作成
+        // GridPane gridPaneSelf = new GridPane();
+        // // Log:ラベルの作成
+        // Label labelLog = createLabel("labelLog", "LOG");
+        // // Log:リストの作成
+        // for (int i = 0; i < 5; i++) {
+        //     listLogAdd("none");
+        // }
+        // // Log:リストラベルの作成
+        // Label labelLog1 = new Label("none");
+        // Label labelLog2 = new Label("none");
+        // Label labelLog3 = new Label("none");
+        // Label labelLog4 = new Label("none");
+        // Label labelLog5 = new Label("none");
         // =====================================================================================================================================
         // =====================================================================================================================================
         // Enemy:CoodinateButtonの設定
         for (int l = 0; l < 5; l++) {
             for (int j = 0; j < 5; j++) {
-                buttonEnemyCoodinates[l][j] = createButtonCoodinate(textsCoodinates[l][j], j, l,
-                        labelActiveEnemyCoodinate,true);
+                GUI_DB.buttonEnemyCoodinates[l][j] = createButtonCoodinate(textsCoodinates[l][j], j, l,
+                        labelActiveEnemyCoodinate, true);
             }
         }
         // // Enemy:Gridpaneの設定
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                GridPane.setConstraints(buttonEnemyCoodinates[i][j], j, i);}
-            
-            gridPaneEnemy.getChildren().addAll(buttonEnemyCoodinates[i]);
+                GridPane.setConstraints(GUI_DB.buttonEnemyCoodinates[i][j], j, i);
+            }
+
+            gridPaneEnemy.getChildren().addAll(GUI_DB.buttonEnemyCoodinates[i]);
         }
         // Enemy:stateButtonの設定
         buttonHit.setOnAction(
-                event -> buttonStateEnemyAction(xEnemy, yEnemy, 2, flagEnemy, labelActiveEnemyCoodinate,
+                event -> buttonStateEnemyAction(GUI_DB.xEnemy, GUI_DB.yEnemy, 2, GUI_DB.flagEnemy,
+                        labelActiveEnemyCoodinate,
                         buttonCalcurateCoodinates));
         buttonNot.setOnAction(
-                event -> buttonStateEnemyAction(xEnemy, yEnemy, 0, flagEnemy, labelActiveEnemyCoodinate,
+                event -> buttonStateEnemyAction(GUI_DB.xEnemy, GUI_DB.yEnemy, 0, GUI_DB.flagEnemy,
+                        labelActiveEnemyCoodinate,
                         buttonCalcurateCoodinates));
         buttonNear.setOnAction(
-                event -> buttonStateEnemyAction(xEnemy, yEnemy, 1, flagEnemy, labelActiveEnemyCoodinate,
+                event -> buttonStateEnemyAction(GUI_DB.xEnemy, GUI_DB.yEnemy, 1, GUI_DB.flagEnemy,
+                        labelActiveEnemyCoodinate,
                         buttonCalcurateCoodinates));
         // Enemy:Moveボタンの設定
         buttonEnemyNorth_1
-                .setOnAction(event -> buttonEnemyMoveAction(0, 1, buttonCalcurateCoodinates, labelEnemylNaval));
+                .setOnAction(event -> buttonEnemyMoveAction(0, 1, buttonCalcurateCoodinates, GUI_DB.labelEnemylNaval));
         buttonEnemyEast_1
-                .setOnAction(event -> buttonEnemyMoveAction(1, 1, buttonCalcurateCoodinates, labelEnemylNaval));
+                .setOnAction(event -> buttonEnemyMoveAction(1, 1, buttonCalcurateCoodinates, GUI_DB.labelEnemylNaval));
         buttonEnemySouth_1
-                .setOnAction(event -> buttonEnemyMoveAction(2, 1, buttonCalcurateCoodinates, labelEnemylNaval));
+                .setOnAction(event -> buttonEnemyMoveAction(2, 1, buttonCalcurateCoodinates, GUI_DB.labelEnemylNaval));
         buttonEnemyWest_1
-                .setOnAction(event -> buttonEnemyMoveAction(3, 1, buttonCalcurateCoodinates, labelEnemylNaval));
+                .setOnAction(event -> buttonEnemyMoveAction(3, 1, buttonCalcurateCoodinates, GUI_DB.labelEnemylNaval));
         // Enemy:Move2ボタンの設定
         buttonEnemyNorth_2
-                .setOnAction(event -> buttonEnemyMoveAction(0, 2, buttonCalcurateCoodinates, labelEnemylNaval));
+                .setOnAction(event -> buttonEnemyMoveAction(0, 2, buttonCalcurateCoodinates, GUI_DB.labelEnemylNaval));
         buttonEnemyEast_2
-                .setOnAction(event -> buttonEnemyMoveAction(1, 2, buttonCalcurateCoodinates, labelEnemylNaval));
+                .setOnAction(event -> buttonEnemyMoveAction(1, 2, buttonCalcurateCoodinates, GUI_DB.labelEnemylNaval));
         buttonEnemySouth_2
-                .setOnAction(event -> buttonEnemyMoveAction(2, 2, buttonCalcurateCoodinates, labelEnemylNaval));
+                .setOnAction(event -> buttonEnemyMoveAction(2, 2, buttonCalcurateCoodinates, GUI_DB.labelEnemylNaval));
         buttonEnemyWest_2
-                .setOnAction(event -> buttonEnemyMoveAction(3, 2, buttonCalcurateCoodinates, labelEnemylNaval));
+                .setOnAction(event -> buttonEnemyMoveAction(3, 2, buttonCalcurateCoodinates, GUI_DB.labelEnemylNaval));
         // Calcurate:ボタンの設定
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
@@ -283,7 +307,7 @@ public class Naval extends Application {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 buttonSelfCoodinates[i][j] = createButtonCoodinate(textsCoodinates[i][j], j, i,
-                        labelActiveSelfCoodinate,false);
+                        labelActiveSelfCoodinate, false);
             }
         }
         // Self:gridpaneの設定
@@ -298,13 +322,19 @@ public class Naval extends Application {
         // Self:Firedボタンの設定
         buttonSelfFired
                 .setOnAction(event -> buttonSelfFiredAction(labelActiveSelfCoodinate, buttonCalcurateCoodinates));
+        // Log:listLogの設定
+        labelLog1.setFont(GUI_DB.fontLog);
+        labelLog2.setFont(GUI_DB.fontLog);
+        labelLog3.setFont(GUI_DB.fontLog);
+        labelLog4.setFont(GUI_DB.fontLog);
+        labelLog5.setFont(GUI_DB.fontLog);
         // =====================================================================================================================================
         // =====================================================================================================================================
         // Enemy:GUIレイアウトを作成
         HBox hboxStateButton = new HBox(buttonHit, buttonNot, buttonNear);
         HBox hBoxMove_1Button = new HBox(buttonEnemyNorth_1, buttonEnemyEast_1, buttonEnemySouth_1, buttonEnemyWest_1);
         HBox hBoxMove_2Button = new HBox(buttonEnemyNorth_2, buttonEnemyEast_2, buttonEnemySouth_2, buttonEnemyWest_2);
-        VBox vBoxEnemy = new VBox(labelEnemylNaval, gridPaneEnemy, labelActiveEnemyCoodinate, hboxStateButton,
+        VBox vBoxEnemy = new VBox(GUI_DB.labelEnemylNaval, gridPaneEnemy, labelActiveEnemyCoodinate, hboxStateButton,
                 hBoxMove_1Button, hBoxMove_2Button);
         // Enemy:GUIレイアウトの設定
         hBoxMove_1Button.setPadding(new Insets(5, 0, 5, 0));
@@ -318,8 +348,11 @@ public class Naval extends Application {
         VBox vBoxSelf = new VBox(labelSelf, gridPaneSelf, labelActiveSelfCoodinate, hBoxSetFired);
         // Self:GUIレイアウトの設定
         vBoxSelf.setPadding(new Insets(10));
+        // Log:GUIレイアウトの作成
+        VBox vBoxlabelLog = new VBox(labelLog1, labelLog2, labelLog3, labelLog4, labelLog5);
+        VBox vBoxLog = new VBox(labelLog, vBoxlabelLog);
         // 全体のレイアウトをマージする
-        HBox hboxEnemyToSelf = new HBox(vBoxEnemy, vboxCalucurate, vBoxSelf);
+        HBox hboxEnemyToSelf = new HBox(vBoxEnemy, vboxCalucurate, vBoxSelf, vBoxLog);
         hboxEnemyToSelf.setSpacing(5.0);
         // =====================================================================================================================================
         // =====================================================================================================================================
